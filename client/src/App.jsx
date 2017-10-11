7306b9f7ed71ec267db67b380fa0611c775193bf
RachelEversmann <Rachel.Eversmann@gamail.com>
2017-10-11 10:17:57 -0500

    fixed linter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Box from 'grommet/components/Box';
 import Menu from 'grommet/components/Menu';
 import Anchor from 'grommet/components/Anchor';
 import Search from 'grommet/components/Search';
-import MenuIcon from 'grommet/components/icons/base/Menu'
+import MenuIcon from 'grommet/components/icons/base/Menu';
 import { connect } from 'react-redux';
 
 class Hackfolio extends Component {
