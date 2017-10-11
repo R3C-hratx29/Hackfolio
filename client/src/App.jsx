ed9f5c22760b5b6c3ee8f1b6c69f1b120891b4c2
RachelEversmann <Rachel.Eversmann@gamail.com>
2017-10-10 19:43:52 -0500

    testing linter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-undef */
-import React, { Component } from 'react';
 import App from 'grommet/components/App';
 import Heading from 'grommet/components/Heading';
 import Header from 'grommet/components/Header';
@@ -8,7 +6,7 @@ import Box from 'grommet/components/Box';
 import Menu from 'grommet/components/Menu';
 import Anchor from 'grommet/components/Anchor';
 import Search from 'grommet/components/Search';
-import MenuIcon from 'grommet/components/icons/base/Menu';
+import MenuIcon from 'grommet/components/icons/base/Menu'
 
 class Hackfolio extends Component {
   constructor() {
