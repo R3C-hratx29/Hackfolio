c32d78ce9bd71cd9d689453ecfa57888fa0cf410
RachelEversmann <Rachel.Eversmann@gamail.com>
2017-10-10 19:53:13 -0500

    testing linter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Title from 'grommet/components/Title';
 import Box from 'grommet/components/Box';
 import Menu from 'grommet/components/Menu';
 import Anchor from 'grommet/components/Anchor';
-import Search from 'grommet/components/Search';
+import Search from 'grommet/components/Search'
 import MenuIcon from 'grommet/components/icons/base/Menu'
 
 class Hackfolio extends Component {
