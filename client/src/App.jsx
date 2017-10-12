634a39116a2f18a82b3248d0dedda7fbffb303f9
RachelEversmann <Rachel.Eversmann@gamail.com>
2017-10-11 19:32:45 -0500

    added some redux stuff, its ugly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,37 @@
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
 import App from 'grommet/components/App';
 import Heading from 'grommet/components/Heading';
 import { connect } from 'react-redux';
+import NavBar from './NavBar';
 // import { Router, Route, Switch} from 'react-router';
 
 class Hackfolio extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      test: false
+      test: true
     };
   }
 
   render() {
     if (this.state.test) {
-      console.log('blah');
+      console.log('ignore me');
     }
+    const obj = {
+      type: 'cat',
+      payload: {
+        blah: 'blahblah'
+      }
+    };
+    if (this.state.test) {
+      this.props.dispatch(obj);
+      this.setState({ test: false });
+    }
+    console.log('actual blah', this.props.blah);
     return (
       <App className="App">
+        <NavBar />
         <Heading>
           Hello World!
         </Heading>
--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,51 +1,53 @@
 /* eslint-disable react/jsx-boolean-value */
-import React from 'react';
+import React, { Component } from 'react';
 import Header from 'grommet/components/Header';
 import Box from 'grommet/components/Box';
-import Menu from 'grommet/components/Menu';
-import Anchor from 'grommet/components/Anchor';
-import Search from 'grommet/components/Search';
-import MenuIcon from 'grommet/components/icons/base/Menu';
+// import Title from 'grommet/components/Title';
+import UserIcon from 'grommet/components/icons/base/User';
+import Button from 'grommet/components/Button';
+import HomeIcon from 'grommet/components/icons/base/Home';
 
-const NavBar = () => (
-
-  <Header
-    fixed={false}
-    float={false}
-    splash={false}
-  >
-    <Box
-      flex={true}
-      justify="end"
-      direction="row"
-      responsive={false}
-    >
-      <Search
-        inline={true}
-        fill={true}
-        size="medium"
-        placeHolder="Search"
-        dropAlign={{ right: 'right' }}
-      />
-      <Menu
-        icon={<MenuIcon />}
-        dropAlign={{ right: 'right' }}
-      >
-        <Anchor
-          href="#"
-          className="active"
-        >
-          First
-        </Anchor>
-        <Anchor href="#">
-          Second
-        </Anchor>
-        <Anchor href="#">
-          Third
-        </Anchor>
-      </Menu>
-    </Box>
-  </Header>
-);
+class NavBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      test: false
+    };
+    this.testFunc = this.testFunc.bind(this);
+  }
+  testFunc(e) {
+    this.setState({ test: true });
+    console.log(this.state.test, e);
+  }
+  render() {
+    return (
+      <div>
+        <Header>
+          <Box
+            flex={true}
+            justify="start"
+            direction="row"
+            responsive={false}
+            colorIndex="brand"
+            pad={{ vertical: 'medium', horizontal: 'small' }}
+          >
+            <Button
+              icon={<UserIcon />}
+              label="Profile"
+              plain={true}
+              onClick={() => { this.testFunc('Profile'); }}
+            />
+            <Button
+              icon={<HomeIcon />}
+              label="Home Page"
+              plain={true}
+              onClick={() => { this.testFunc('Home Page'); }}
+            />
+          </Box>
+        </Header>
+      </div>
+    );
+  }
+}
 
 export default NavBar;
