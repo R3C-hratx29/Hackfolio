d6f8065cee625daa53cf7ec486fd4fb7abb26f3f
RachelEversmann <Rachel.Eversmann@gamail.com>
2017-10-11 20:20:51 -0500

    fixed lint error

--- client/src/Projects.jsx
+++ client/src/Projects.jsx
@@ -1,52 +1,58 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-//Grommet Components
+// import PropTypes from 'prop-types';
+/* eslint-disable react/jsx-boolean-value */
+// Grommet Components
 import Tiles from 'grommet/components/Tiles';
 import Tile from 'grommet/components/Tile';
 import Card from 'grommet/components/Card';
 import Anchor from 'grommet/components/Anchor';
 import Box from 'grommet/components/Box';
 
-//Grommet Icons
+// Grommet Icons
 import LinkNextIcon from 'grommet/components/icons/base/LinkNext';
 import GithubIcon from 'grommet/components/icons/base/SocialGithub';
 
-//Component Styles
+// Component Styles
 import './Projects.scss';
 
 class Projects extends React.Component {
-  static propTypes = {
-
+  constructor() {
+    super();
+    this.state = {
+      test: false
+    };
   }
-
   render() {
-
+    console.log(this.state.test);
+    this.setState({ test: true });
     return (
       <div className="Projects">
-        <Tiles fill={true}
-          flush={false}>
+        <Tiles
+          fill={true}
+          flush={false}
+        >
           <Tile>
-            <Card thumbnail='//cdn.dribbble.com/users/33073/screenshots/2425824/800.png'
-              heading='Read Books'
-              description='Sample description providing more details.'
+            <Card
+              thumbnail="//cdn.dribbble.com/users/33073/screenshots/2425824/800.png"
+              heading="Read Books"
+              description="Sample description providing more details."
               link={
-                <Box direction='row' justify='between'>
+                <Box direction="row" justify="between">
                   <Anchor
                     icon={<GithubIcon />}
-                    label='GitHub'
-                    href='#'
+                    label="GitHub"
+                    href="#"
                     primary={true}
                     reverse={false}
-                    target='blank'
+                    target="blank"
                   />
                   <Anchor
                     icon={<LinkNextIcon />}
-                    label='Visit Site'
-                    href='#'
+                    label="Visit Site"
+                    href="#"
                     primary={true}
                     reverse={false}
-                    target='blank'
+                    target="blank"
                   />
                 </Box>
               }
@@ -54,7 +60,7 @@ class Projects extends React.Component {
           </Tile>
         </Tiles>
       </div>
-    )
+    );
   }
 }
 
