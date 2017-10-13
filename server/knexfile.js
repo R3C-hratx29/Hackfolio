d05aebb624ffd273d337b3de56e3a493ec05a3b1
Charlie <ctbwilhite@gmail.com>
2017-10-13 16:04:47 -0500

    Setting up deployment variable for heroku

---
+++ server/knexfile.js
@@ -0,0 +1,8 @@
+const app = require('./app')
+
+module.exports = {
+  client: ,
+  connection: {
+    database: "postgres://gcyiqiuxivopmw:7731d9c2ff6613963d5926e91a39b5deb5f7c51a233f7766d7f5408a7e8fa3b0@ec2-54-243-255-57.compute-1.amazonaws.com:5432/d7qklm1psmuo68"
+  }
+}
