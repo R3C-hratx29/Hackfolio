bd345f60796cdcc50b2564c6d3d6317e9d3e936c
Charlie <ctbwilhite@gmail.com>
2017-10-14 13:38:07 -0500

    Db schema completed

--- server/migrations/20171014103612_hackfolio.js
+++ server/migrations/20171014103612_hackfolio.js
@@ -4,17 +4,38 @@ exports.up = (knex, Promise) => {
       table.increments('uid').primary();
       table.string('username');
       table.string('password');
-      table.string('name');
       table.string('email');
-      table.string('profession');
-      table.string('profile_pic');
-      table.timestamps();
     }),
+    knex.schema.createTable('profiles', (table) => {
+      table.increments('id').primary();
+      table.integer('user_id')
+           .references('uid')
+           .inTable('users');
+      table.string('bio');
+      table.string('profile_pic');
+      table.string('profession');
+      table.string('name');
+      table.specificType('links', 'jsonb[]');
+    }),
+    knex.schema.createTable('projects', (table) => {
+      table.increments('id').primary();
+      table.integer('user_id')
+           .references('uid')
+           .inTable('users');
+      table.string('title');
+      table.string('description');
+      table.string('github_link');
+      table.string('website_link');
+      table.specificType('stack', 'jsonb[]');
+      table.specificType('images', 'jsonb[]');
+    })
   ]);
 };
 
 exports.down = (knex, Promise) => {
   return Promise.all([
     knex.schema.dropTable('users'),
+    knex.schema.dropTable('profiles'),
+    knex.schema.dropTable('projects')
   ]);
 };
