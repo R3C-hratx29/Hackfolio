ce5571b40511b072599d06914f525133e78dee2a
Ross Waycaster <rosswaycaster@me.com>
2017-10-16 15:46:01 -0500

    moved Project Card to it`s own component

--- client/src/components/AddProject.jsx
+++ client/src/components/AddProject.jsx
@@ -1,14 +1,84 @@
 import React from 'react';
-import Layer from 'grommet/components/Layer';
 import PropTypes from 'prop-types';
 
-const AddProject = (props) => {
-  return (
-    <Layer closer onClose={props.toggleProjectModal} hidden={props.hideProjectModal}>
-      Hi
-    </Layer>
-  );
-};
+// Grommet Components
+import Box from 'grommet/components/Box';
+import Layer from 'grommet/components/Layer';
+import Form from 'grommet/components/Form';
+import FormField from 'grommet/components/FormField';
+import TextInput from 'grommet/components/TextInput';
+import Header from 'grommet/components/Header';
+import Heading from 'grommet/components/Heading';
+
+// Custom Components
+import ProjectCard from './ProjectCard';
+
+// Component Styles
+import './../styles/ProjectCard.scss';
+
+class AddProject extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      project: {
+        title: '',
+        description: '',
+        github_link: '',
+        website_link: '',
+        images: [],
+        stack: [],
+      }
+    };
+
+    this.updateProject = this.updateProject.bind(this);
+  }
+
+  updateProject(state) {
+    this.setState({
+      project: Object.assign(
+        {},
+        this.state.project,
+        state,
+      )
+    });
+  }
+
+  render() {
+    return (
+      <Layer closer onClose={this.props.toggleProjectModal} hidden={this.props.hideProjectModal}>
+        <Box direction="row" responsive={false} pad={{ horizontal: 'large', vertical: 'large', between: 'large' }}>
+          <ProjectCard project={this.state.project} />
+          <Form>
+            <Header>
+              <Heading>
+                Add a Project
+              </Heading>
+            </Header>
+            <FormField label="Title">
+              <TextInput
+                onDOMChange={
+                  (e) => {
+                    this.updateProject({ title: e.target.value });
+                  }
+                }
+              />
+            </FormField>
+            <FormField label="Description">
+              <TextInput
+                onDOMChange={
+                  (e) => {
+                    this.updateProject({ description: e.target.value });
+                  }
+                }
+              />
+            </FormField>
+          </Form>
+        </Box>
+      </Layer>
+    );
+  }
+}
 
 AddProject.propTypes = {
   toggleProjectModal: PropTypes.func.isRequired,
--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -2,35 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-
 // Grommet Components
 import Tiles from 'grommet/components/Tiles';
-import Tile from 'grommet/components/Tile';
-import Card from 'grommet/components/Card';
-import Anchor from 'grommet/components/Anchor';
 import Box from 'grommet/components/Box';
-import Carousel from 'grommet/components/Carousel';
-import Image from 'grommet/components/Image';
-import Heading from 'grommet/components/Heading';
 import Button from 'grommet/components/Button';
 
 // Grommet Icons
-import LinkNextIcon from 'grommet/components/icons/base/LinkNext';
-import GithubIcon from 'grommet/components/icons/base/SocialGithub';
 import AddIcon from 'grommet/components/icons/base/Add';
 
+// Custom Components
+import ProjectCard from './ProjectCard';
 import AddProject from './AddProject';
 import ProfileBox from './ProfileBox';
-
-// Component Styles
-import './../styles/Projects.scss';
 
 class Projects extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      hideProjectModal: true
+      hideProjectModal: false
     };
 
     this.toggleProjectModal = this.toggleProjectModal.bind(this);
@@ -44,82 +34,7 @@ class Projects extends React.Component {
 
   render() {
     const projects = this.props.userProfile.projects.map((project) => (
-      <Tile
-        key={project.id}
-        full={false}
-      >
-        {
-          project.images.length <= 1 ? (
-            <Image
-              size="medium"
-              style={{ maxWidth: 384, maxHeight: 280 }}
-              src={project.images[0]}
-            />
-          ) : (
-            <Box
-              size="medium"
-            >
-              <Carousel
-                autoplay={false}
-                style={{ maxWidth: 390, maxHeight: 284 }}
-              >
-                {
-                  project.images.map((image) => (
-                    <Image
-                      key={image}
-                      size="medium"
-                      fit="cover"
-                      src={image}
-                    />
-                  ))
-                }
-              </Carousel>
-            </Box>
-          )
-        }
-        <Card
-          contentPad="medium"
-          heading={project.title}
-          description={
-            <div>
-              <Heading tag="h3" className="description">
-                {project.description}
-              </Heading>
-              <div className="stack">
-                {
-                  project.stack.map((el) => {
-                    return <div key={el}>{el}</div>;
-                  })
-                }
-              </div>
-            </div>
-          }
-          link={
-            <Box
-              direction="row"
-              justify="between"
-              responsive={false}
-            >
-              <Anchor
-                icon={<GithubIcon />}
-                label="GitHub"
-                href={project.github_link}
-                primary
-                reverse={false}
-                target="blank"
-              />
-              <Anchor
-                icon={<LinkNextIcon />}
-                label="Visit Site"
-                href={project.website_link}
-                primary
-                reverse={false}
-                target="blank"
-              />
-            </Box>
-          }
-        />
-      </Tile>
+      <ProjectCard project={project} />
     ));
 
     return (
