38b6d23109bd1da80a9905fe3b338b212d865cb9
Ross Waycaster <rosswaycaster@me.com>
2017-10-17 14:00:05 -0500

    add image uploading to firebase

--- client/src/components/AddProject.jsx
+++ client/src/components/AddProject.jsx
@@ -1,5 +1,8 @@
+/*  eslint no-underscore-dangle: "error"  */
 import React from 'react';
 import PropTypes from 'prop-types';
+import firebase from 'firebase';
+import FileUploader from 'react-firebase-file-uploader';
 
 // Grommet Components
 import Box from 'grommet/components/Box';
@@ -15,12 +18,24 @@ import Split from 'grommet/components/Split';
 
 // Grommet Icons
 import ImageIcon from 'grommet/components/icons/base/Image';
+import Spinning from 'grommet/components/icons/Spinning';
 
 // Custom Components
 import ProjectCard from './ProjectCard';
 
 // Component Styles
 import './../styles/ProjectCard.scss';
+
+// Initalize Firebase
+const config = {
+  apiKey: 'AIzaSyDbSmVKg6UsDZFa2LHTqqm4Q9hPylorbao',
+  authDomain: 'hackfolio-ed6a4.firebaseapp.com',
+  databaseURL: 'https://hackfolio-ed6a4.firebaseio.com',
+  projectId: 'hackfolio-ed6a4',
+  storageBucket: 'hackfolio-ed6a4.appspot.com',
+  messagingSenderId: '977473242483'
+};
+firebase.initializeApp(config);
 
 class AddProject extends React.Component {
   constructor(props) {
@@ -34,12 +49,23 @@ class AddProject extends React.Component {
         website_link: '',
         images: [],
         stack: [],
-      }
+      },
+      uploading: false
     };
 
     this.updateProject = this.updateProject.bind(this);
     this.addImageURL = this.addImageURL.bind(this);
     this.updateImages = this.updateImages.bind(this);
+    this.onImageUpload = this.onImageUpload.bind(this);
+  }
+
+  onImageUpload(file) {
+    this.menuRef.setState({ state: 'collapsed' });
+    firebase.storage().ref('images').child(file).getDownloadURL()
+      .then(url => {
+        this.addImageURL(url);
+        this.setState({ uploading: false });
+      });
   }
 
   updateProject(state) {
@@ -61,9 +87,17 @@ class AddProject extends React.Component {
     });
   }
 
-  addImageURL() {
+  addImageURL(url = '') {
     const array = this.state.project.images;
-    array.push('');
+    array.push(url);
+    this.updateProject({
+      images: array
+    });
+  }
+
+  removeImage(index) {
+    const array = this.state.project.images;
+    array.splice(index, 1);
     this.updateProject({
       images: array
     });
@@ -101,81 +135,113 @@ class AddProject extends React.Component {
                   label="Add Image"
                   inline={false}
                   reverse
+                  ref={ref => { this.menuRef = ref; }}
                 >
                   <Anchor
-                    onClick={this.addImageURL}
+                    onClick={() => this.addImageURL('')}
                   >
                     Image URL
                   </Anchor>
                   <Anchor
-                    onClick={() => {}}
+                    onClick={(e) => { e.stopPropagation(); }}
                   >
-                    Upload Image
+                    <label htmlFor="firebaseUpload">
+                      { this.state.uploading && <Spinning /> } Upload Image
+                      <FileUploader
+                        style={{ display: 'none' }}
+                        hidden
+                        id="firebaseUpload"
+                        accept="image/*"
+                        randomizeFilename
+                        storageRef={firebase.storage().ref('images')}
+                        onUploadStart={() => { this.setState({ uploading: true }); }}
+                        onUploadSuccess={this.onImageUpload}
+                      />
+                    </label>
                   </Anchor>
                 </Menu>
               </Header>
-              <FormField label="Title">
-                <TextInput
-                  onDOMChange={
-                    (e) => {
-                      this.updateProject({ title: e.target.value });
+              <div className="formScroll">
+                <FormField label="Title">
+                  <TextInput
+                    onDOMChange={
+                      (e) => {
+                        this.updateProject({ title: e.target.value });
+                      }
                     }
-                  }
-                />
-              </FormField>
-              <FormField label="Description">
-                <TextInput
-                  onDOMChange={
-                    (e) => {
-                      this.updateProject({ description: e.target.value });
+                  />
+                </FormField>
+                <FormField label="Description">
+                  <TextInput
+                    onDOMChange={
+                      (e) => {
+                        this.updateProject({ description: e.target.value });
+                      }
                     }
-                  }
-                />
-              </FormField>
-              <FormField label="Website Link">
-                <TextInput
-                  onDOMChange={
-                    (e) => {
-                      this.updateProject({ website_link: e.target.value });
+                  />
+                </FormField>
+                <FormField label="Website Link">
+                  <TextInput
+                    onDOMChange={
+                      (e) => {
+                        this.updateProject({ website_link: e.target.value });
+                      }
                     }
-                  }
-                />
-              </FormField>
-              <FormField label="Github Link">
-                <TextInput
-                  onDOMChange={
-                    (e) => {
-                      this.updateProject({ github_link: e.target.value });
+                  />
+                </FormField>
+                <FormField label="Github Link">
+                  <TextInput
+                    onDOMChange={
+                      (e) => {
+                        this.updateProject({ github_link: e.target.value });
+                      }
                     }
-                  }
-                />
-              </FormField>
-              <FormField label="Stack">
-                <TextInput
-                  placeHolder="Separate with commas"
-                  onDOMChange={
-                    (e) => {
-                      this.updateStack(e.target.value);
+                  />
+                </FormField>
+                <FormField label="Stack">
+                  <TextInput
+                    placeHolder="Separate with commas"
+                    onDOMChange={
+                      (e) => {
+                        this.updateStack(e.target.value);
+                      }
                     }
-                  }
-                />
-              </FormField>
-              {
-                this.state.project.images.map((image, index) => {
-                  const i = index;
-                  return (
-                    <FormField key={i} label={`Image #${index + 1}`}>
-                      <TextInput
-                        onDOMChange={
-                          (e) => {
-                            this.updateImages(index, e.target.value);
-                          }
+                  />
+                </FormField>
+                {
+                  this.state.project.images.map((image, index) => {
+                    const i = index;
+                    return (
+                      <FormField
+                        key={i}
+                        label={
+                          <div>
+                            <span>Image #{index + 1}</span>
+                            <span
+                              tabIndex={0}
+                              className="deleteImage"
+                              onClick={() => this.removeImage(index)}
+                              onKeyPress={() => {}}
+                              role="button"
+                            >
+                              Delete
+                            </span>
+                          </div>
                         }
-                      />
-                    </FormField>
-                  );
-                })
-              }
+                      >
+                        <TextInput
+                          value={image}
+                          onDOMChange={
+                            (e) => {
+                              this.updateImages(index, e.target.value);
+                            }
+                          }
+                        />
+                      </FormField>
+                    );
+                  })
+                }
+              </div>
             </Form>
           </Split>
         </Box>
