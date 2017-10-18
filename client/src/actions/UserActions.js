5c2f59d68368609ca6c621130cf9475f843af9fd
RachelEversmann <Rachel.Eversmann@gamail.com>
2017-10-18 11:21:53 -0500

    added successful login

--- client/src/actions/UserActions.js
+++ client/src/actions/UserActions.js
@@ -1,33 +1,47 @@
 /* eslint-disable no-console */
-// import axios from 'axios';
+import axios from 'axios';
 
-export const login = (userdata) => {
-  console.log('login', userdata);
-  // do stuff
+const setUser = (data) => {
   return {
     type: 'SET_CURRENT_USER',
-    userdata
+    payload: {
+      user: data
+    }
   };
 };
 
+export const login = (userdata) => {
+  return ((dispatch) => {
+    return axios.post('/login', {
+      username: userdata.username,
+      password: userdata.password
+    })
+      .then((res) => {
+        dispatch(setUser(res.data));
+      })
+      .catch((err) => {
+        throw err;
+      });
+  });
+};
+
 export const signup = (userdata) => {
-  console.log('signup', userdata);
-  /* axios.post('/signup', {
-    params: {
+  return (dispatch) => {
+    return axios.post('/signup', {
       username: userdata.username,
       password: userdata.password,
       email: userdata.email
-    }
-  })
-  .then(  ) */
-  return {
-    type: 'SET_CURRENT_USER',
-    userdata
+    })
+      .then((res) => {
+        dispatch(setUser(res.data));
+      })
+      .catch((err) => {
+        throw err;
+      });
   };
 };
 
 export const logout = () => {
-  console.log('logged out');
   return {
     type: 'SET_CURRENT_USER',
     payload: {
@@ -36,8 +50,7 @@ export const logout = () => {
   };
 };
 
-export const search = (string) => {
-  console.log('search', string);
+export const search = () => {
   return {
     type: 'SET_USER_PROFILE',
     payload: {
