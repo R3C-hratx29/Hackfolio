56b07e36cdb07f18c796eb776eeb25e8eebde961
Charlie <ctbwilhite@gmail.com>
2017-10-20 11:49:03 -0500

    making changes to db connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const api = require('./api');
+const dotenv = require('dotenv').config({ path: '../env.env' });
 const bodyParser = require('body-parser');
 const router = require('./routes');
 // const db = require('./models/db');
