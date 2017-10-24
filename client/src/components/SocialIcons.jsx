9558eaf6098e1b20baf2a2dd88de44c779731c35
Ross Waycaster <rosswaycaster@me.com>
2017-10-24 10:35:28 -0500

    social icons

--- client/src/components/SocialIcons.jsx
+++ client/src/components/SocialIcons.jsx
@@ -24,23 +24,43 @@ const SocialIcons = (props) => (
   <Box
     direction="row"
   >
-    {props.socialLinks
-          .filter((social) => social.link)
-          .map((social) =>
-            (
-              <Anchor
-                key={social.link}
-                icon={SocialIcon[social.title]}
-                href={social.link}
-              />
-            ))
+    {props.github &&
+      <Anchor
+        icon={SocialIcon.github}
+        href={props.github}
+        target="_blank"
+      />
+    }
+    {props.twitter &&
+      <Anchor
+        icon={SocialIcon.twitter}
+        href={props.twitter}
+        target="_blank"
+      />
+    }
+    {props.facebook &&
+      <Anchor
+        icon={SocialIcon.facebook}
+        href={props.facebook}
+        target="_blank"
+      />
+    }
+    {props.linked_in &&
+      <Anchor
+        icon={SocialIcon.linked_in}
+        href={props.linked_in}
+        target="_blank"
+      />
     }
   </Box>
 );
 
 const mapStateToProps = (state) => {
   return {
-    socialLinks: state.userProfile.socialLinks
+    github: state.userProfile.github,
+    twitter: state.userProfile.twitter,
+    linked_in: state.userProfile.github,
+    facebook: state.userProfile.facebook,
   };
 };
 export default connect(mapStateToProps)(SocialIcons);
