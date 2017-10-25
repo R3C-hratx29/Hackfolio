0c4f2de54a011e43d6f1fedf2e3440657c916b51
Ross Waycaster <rosswaycaster@me.com>
2017-10-25 15:12:49 -0500

    fix 404 error when loading user profile

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -59,7 +59,7 @@ class Hackfolio extends React.Component {
             <Route path="/user/:id" component={Profile} />
             <Route path="/search" component={SearchPage} />
             <Route path="/chat" component={Chat} />
-            <Route path="*" status={404} component={() => <div>Not Found</div>} />
+            <Route path="*" component={() => <div>Not Found</div>} />
           </Switch>
         </ConnectedRouter>
       </App>
