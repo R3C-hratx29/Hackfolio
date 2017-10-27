4fb092bf7db3d1b2d7972a94083437bcad0342be
rhiroy <rhiroy@protonmail.ch>
2017-10-27 16:03:21 -0500

    starting to connect to redux

--- client/src/components/Bounty/AddBountyCardLayer.jsx
+++ client/src/components/Bounty/AddBountyCardLayer.jsx
@@ -8,6 +8,7 @@ import {
   TextInput,
   Header,
   Heading,
+  Button
 } from 'grommet';
 
 class AddBountyCardLayer extends React.Component {
@@ -15,38 +16,50 @@ class AddBountyCardLayer extends React.Component {
     super(props);
 
     this.state = {
-      bounty_title: '',
-      description: '',
-      price: '',
-      tech_stack: '',
-      github: '',
-      website: '',
-      images: [],
+      bounty: {
+        bounty_title: '',
+        description: '',
+        price: '',
+        tech_stack: '',
+        github: '',
+        website: '',
+        images: [],
+      }
     };
+
+    this.saveChanges = this.saveChanges.bind(this);
+    this.updateBounty = this.updateBounty.bind(this);
   }
-  saveChanges() {
-    this.props.saveChanges({
-      bounty_title: this.state.bounty_title,
-      description: this.state.description,
-      price: this.state.price,
-      tech_stack: this.state.tech_stack,
-      github: this.state.github,
-      website: this.state.website,
-      images: this.state.images
+
+  saveChanges() { this.props.saveChanges(this.state.bounty); }
+  /* saveChanges() {
+   *   this.props.saveChanges({
+   *     bounty_title: this.state.bounty.bounty_title,
+   *     description: this.state.bounty.description,
+   *     price: this.state.bounty.price,
+   *     tech_stack: this.state.bounty.tech_stack,
+   *     github: this.state.bounty.github,
+   *     website: this.state.bounty.website,
+   *     images: this.state.bounty.images
+   *   });
+   * }
+   */
+
+  updateBounty(state) {
+    this.setState({
+      bounty: Object.assign({}, this.state.bounty, state)
     });
   }
 
   render() {
     return (
       <Layer
-        hidden={this.props.hidden}
+        className="AddBounty"
         closer
         onClose={this.props.hideBountyLayerFunction}
+        hidden={this.props.hidden}
       >
-        <Box
-          direction="row"
-          pad={{ vertical: 'large' }}
-        >
+        <Box direction="row" pad={{ vertical: 'large' }}>
           <Form>
             <Header
               justify="between"
@@ -55,52 +68,68 @@ class AddBountyCardLayer extends React.Component {
                 Add a bounty
               </Heading>
             </Header>
-            <FormField
-              label="Give your bounty a name"
-            >
+            <FormField label="Give your bounty a name">
               <TextInput
+                value={this.state.bounty.bounty_title}
+                onDOMChange={e => {
+                    this.updateBounty({ bounty_title: e.target.value });
+                }}
                 placeholder="bounty name"
               />
             </FormField>
-            <FormField
-              label="Describe the task you want help completing"
-            >
+            <FormField label="Describe the task you want help completing">
               <TextInput
+                value={this.state.bounty.description}
+                onDOMChange={e => {
+                    this.updateBounty({ description: e.target.value });
+                }}
                 placeholder="bounty description"
               />
             </FormField>
-            <FormField
-              label="Name your price: how much are you offering to complete this task?"
-            >
+            <FormField label="Name your price: how much are you offering to complete this task?">
               <TextInput
+                value={this.state.bounty.price}
+                onDOMChange={e => {
+                    this.updateBounty({ price: e.target.value });
+                }}
                 placeholder="$$$"
               />
             </FormField>
-            <FormField
-              label="List the tech stack that developers will need to complete the task"
-            >
+            <FormField label="List the tech stack that developers will need to complete the task">
               <TextInput
+                value={this.state.bounty.tech_stack}
+                onDOMChange={e => {
+                    this.updateBounty({ tech_stack: e.target.value });
+                }}
                 placeholder="tech stack"
               />
             </FormField>
-            <FormField
-              label="Link to your github if this isn't a brand new project"
-            >
+            <FormField label="Link to your github if this isn't a brand new project">
               <TextInput
+                value={this.state.bounty.github}
+                onDOMChange={e => {
+                    this.updateBounty({ github: e.target.value });
+                }}
                 placeholder="github link"
               />
             </FormField>
-            <FormField
-              label="Link to your website"
-            >
+            <FormField label="Link to your website">
               <TextInput
+                value={this.state.bounty.website}
+                onDOMChange={e => {
+                    this.updateBounty({ website: e.target.value });
+                }}
                 placeholder="website link"
               />
             </FormField>
+            <Box>
+              <Button primary fill onClick={this.saveChanges} label="Save Bounty" />
+            </Box>
           </Form>
         </Box>
       </Layer>
     );
   }
 }
+
 export default AddBountyCardLayer;
