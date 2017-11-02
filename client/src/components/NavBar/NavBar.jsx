3294dccb60346b3130ecd4de1fc05a1d7f43c00a
Ross Waycaster <rosswaycaster@me.com>
2017-11-02 18:59:51 -0500

    moved notifications icon

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,21 +1,11 @@
 /* eslint-disable import/first */
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { push } from 'react-router-redux';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { push } from "react-router-redux";
 
 // Grommet Imports
-import {
-  Header,
-  Box,
-  Tip,
-  TextInput,
-  Button,
-  Form,
-  FormField,
-  Menu,
-  Anchor
-} from 'grommet';
+import { Header, Box, Tip, TextInput, Button, Form, FormField, Menu, Anchor } from "grommet";
 
 // Grommet Icons
 import {
@@ -25,24 +15,24 @@ import {
   LogoutIcon,
   MultipleIcon,
   BookmarkIcon,
-} from 'grommet/components/icons/base';
+} from "grommet/components/icons/base";
 
 // Custom Imports
-import Notifications from './Notifications';
-import Modal from './Modal';
-import * as UserAction from './../../actions/UserActions';
-import { getProfile } from './../../actions/ProfileActions';
+import Notifications from "./Notifications";
+import Modal from "./Modal";
+import * as UserAction from "./../../actions/UserActions";
+import { getProfile } from "./../../actions/ProfileActions";
 
 // Custom Styles
-import './../../styles/NavBar.scss';
+import "./../../styles/NavBar.scss";
 
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchText: '',
+      searchText: "",
       help: false,
-      modalState: false
+      modalState: false,
     };
     this.searchHandler = this.searchHandler.bind(this);
     this.sendSearch = this.sendSearch.bind(this);
@@ -64,7 +54,7 @@ class NavBar extends React.Component {
     e.preventDefault();
     const temp = this.state.searchText;
     this.props.search(temp);
-    this.setState({ searchText: '' });
+    this.setState({ searchText: "" });
   }
 
   goProfile() {
@@ -73,7 +63,7 @@ class NavBar extends React.Component {
   }
 
   goHome() {
-    this.props.goToHome('/');
+    this.props.goToHome("/");
   }
 
   toggleModal() {
@@ -84,9 +74,7 @@ class NavBar extends React.Component {
   render() {
     return (
       <div className="NavBar">
-        <Header
-          style={{ marginBottom: 22 }}
-        >
+        <Header style={{ marginBottom: 22 }}>
           <Box
             flex
             justify="between"
@@ -94,69 +82,29 @@ class NavBar extends React.Component {
             direction="row"
             responsive
             colorIndex="brand"
-            pad={{ vertical: 'medium', horizontal: 'small' }}
+            pad={{ vertical: "medium", horizontal: "small" }}
           >
-            <Box
-              id="tabs"
-              flex
-              justify="start"
-              align="center"
-              direction="row"
-            >
+            <Box id="tabs" flex justify="start" align="center" direction="row">
               <Box
-                style={{ fontSize: 28, fontWeight: 'bold', margin: '0 15px' }}
+                style={{ fontSize: 28, fontWeight: "bold", margin: "0 15px" }}
                 direction="row"
                 onClick={this.goHome}
               >
-                <Box
-                  justify="center"
-                >
-                  <BookmarkIcon />
-                </Box>
-                <Box
-                  justify="center"
-                >
-                  Hackfolio
-                </Box>
+                <Box justify="center">{this.props.user.username && <Notifications />}</Box>
+                <Box justify="center">Hackfolio</Box>
               </Box>
-              <Button
-                icon={<MultipleIcon />}
-                label="Bounty Board"
-                plain
-                onClick={this.goHome}
-              />
-              { this.props.user.username &&
-                <Menu
-                  label="Profile"
-                  icon={<UserIcon />}
-                >
-                  <Anchor
-                    onClick={this.goProfile}
-                    label="Your Profile"
-                  />
-                  <Anchor
-                    onClick={this.props.goToConversations}
-                    label="Your Conversations"
-                  />
-                  <Anchor
-                    onClick={this.props.goToFave}
-                    label="Your Favorite Bounties"
-                  />
+              <Button icon={<MultipleIcon />} label="Bounty Board" plain onClick={this.goHome} />
+              {this.props.user.username && (
+                <Menu label="Profile" icon={<UserIcon />}>
+                  <Anchor onClick={this.goProfile} label="Your Profile" />
+                  <Anchor onClick={this.props.goToConversations} label="Your Conversations" />
+                  <Anchor onClick={this.props.goToFave} label="Your Favorite Bounties" />
                 </Menu>
-              }
-              { this.props.user.username &&
-                <Notifications />
-              }
+              )}
             </Box>
-            <Box
-              separator="all"
-              direction="row"
-              primary={false}
-            >
-              <Form onSubmit={this.sendSearch} compact >
-                <Box
-                  direction="row"
-                >
+            <Box separator="all" direction="row" primary={false}>
+              <Form onSubmit={this.sendSearch} compact>
+                <Box direction="row">
                   <FormField className="SearchInput">
                     <TextInput
                       placeHolder="Search"
@@ -164,40 +112,34 @@ class NavBar extends React.Component {
                       value={this.state.searchText}
                     />
                   </FormField>
-                  <Button
-                    className="SearchIcon"
-                    icon={<SearchIcon />}
-                    type="submit"
-                    plain
-                  />
+                  <Button className="SearchIcon" icon={<SearchIcon />} type="submit" plain />
                 </Box>
               </Form>
             </Box>
             <Box>
-              { this.props.user === undefined || this.props.user.jwt === undefined ?
-                <Button label="Login" plain icon={<LoginIcon />} onClick={this.toggleModal} /> :
+              {this.props.user === undefined || this.props.user.jwt === undefined ? (
+                <Button label="Login" plain icon={<LoginIcon />} onClick={this.toggleModal} />
+              ) : (
                 <Button label="Logout" plain icon={<LogoutIcon />} onClick={this.props.logout} />
-              }
+              )}
             </Box>
           </Box>
         </Header>
         {this.state.modalState ? <Modal func={this.toggleModal} /> : <div />}
-        { this.state.help && this.props.help === 'Search' ?
-          <Tip
-            target="SearchBar"
-            onClose={() => this.props.displayHelp('Home')}
-          >
+        {this.state.help && this.props.help === "Search" ? (
+          <Tip target="SearchBar" onClose={() => this.props.displayHelp("Home")}>
             Here you can search for other users
-          </Tip> : <div />
-        }
-        { this.state.help && this.props.help === 'Tabs' ?
-          <Tip
-            target="tabs"
-            onClose={() => this.props.displayHelp('Search')}
-          >
+          </Tip>
+        ) : (
+          <div />
+        )}
+        {this.state.help && this.props.help === "Tabs" ? (
+          <Tip target="tabs" onClose={() => this.props.displayHelp("Search")}>
             These will take you to the varies pages of Hackfolio
-          </Tip> : <div />
-        }
+          </Tip>
+        ) : (
+          <div />
+        )}
       </div>
     );
   }
@@ -205,7 +147,7 @@ class NavBar extends React.Component {
 
 NavBar.defaultProps = {
   user: {},
-  help: 'off'
+  help: "off",
 };
 
 NavBar.propTypes = {
@@ -220,22 +162,28 @@ NavBar.propTypes = {
   displayHelp: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     user: state.currentUser,
-    help: state.help
+    help: state.help,
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    search: (text) => { dispatch(UserAction.search(text)); dispatch(push('/search')); },
-    goToHome: (path) => dispatch(push(path)),
-    goToProfile: (user) => { dispatch(getProfile(user)); dispatch(push(`/user/${user}`)); },
-    goToFave: () => dispatch(push('/FavoriteBounties')),
-    goToConversations: () => dispatch(push('/Conversations')),
+    search: text => {
+      dispatch(UserAction.search(text));
+      dispatch(push("/search"));
+    },
+    goToHome: path => dispatch(push(path)),
+    goToProfile: user => {
+      dispatch(getProfile(user));
+      dispatch(push(`/user/${user}`));
+    },
+    goToFave: () => dispatch(push("/FavoriteBounties")),
+    goToConversations: () => dispatch(push("/Conversations")),
     logout: () => dispatch(UserAction.logout()),
-    displayHelp: (next) => dispatch(UserAction.help(next)),
+    displayHelp: next => dispatch(UserAction.help(next)),
   };
 };
 
